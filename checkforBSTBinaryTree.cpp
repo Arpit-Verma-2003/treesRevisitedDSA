d464da7cc96d2d9d35bcae22785e67601f4215d0
Arpit Verma <arpitverma200316@gmail.com>
2024-07-01 15:03:42 +0530

    Create checkforBSTBinaryTree.cpp

---
+++ checkforBSTBinaryTree.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    // Function to check whether a Binary Tree is BST or not.
+    bool check(Node* root,int min, int max){
+        if(root==NULL) return true;
+        if(root->data<=min||root->data>=max) return false;
+        return check(root->left,min,root->data)&&check(root->right,root->data,max);
+    }
+    bool isBST(Node* root) {
+        // Your code here
+        return check(root,INT_MIN,INT_MAX);
+    }
+};
